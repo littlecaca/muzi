b55f44d40a7e9daaa38f973a8358f7b5e4a60772
Mobeidexue <fyli88@qq.com>
2024-08-07 16:16:38 +0800

    More improve timestamp_test.cc

--- muzi/base/test/timestamp_test.cc
+++ muzi/base/test/timestamp_test.cc
@@ -1,21 +1,48 @@
 #include "timestamp.h"
 
-
-#include <unistd.h>
+#include <algorithm>
 #include <iostream>
+#include <unistd.h>
+#include <vector>
 
 #include "timezone.h"
+#include "logger.h"
 
 
 int main(int argc, char const *argv[])
 {
-    std::cout << muzi::TimeStamp().ToFormatString() << std::endl;
-    muzi::TimeStamp::SetTimeZone(&muzi::kLocalTimeZone);
-    sleep(1);
-    std::cout << muzi::TimeStamp().ToFormatString() << std::endl;
-    
-    std::cout << muzi::TimeStamp(12345).ToFormatString() << std::endl;
+    muzi::TimeStamp sum(0);
+
+    for (int k = 0; k < 200; ++k)
+    {
+        muzi::TimeStamp start;
+
+        std::cout << muzi::TimeStamp().ToFormatString() << std::endl;
+        muzi::TimeStamp::SetTimeZone(&muzi::kLocalTimeZone);
+        std::cout << muzi::TimeStamp().ToFormatString() << std::endl;
+        
+        std::cout << muzi::TimeStamp(12345).ToFormatString() << std::endl;
+        
+        std::vector<muzi::TimeStamp> tsv;
+        for (int i = 0; i < 1000; ++i)
+        {
+            tsv.emplace_back();
+        }
+
+        int base = 1000;
+        for (auto &ts : tsv)
+        {
+            ts.AddTime(base);
+            --base;
+        }
+
+        sort(tsv.begin(), tsv.end());
+
+        muzi::TimeStamp end;
+        sum += (end - start);
+    }
     
 
+    LOG_TRACE << sum.GetUsecs() / 200;
     return 0;
 }

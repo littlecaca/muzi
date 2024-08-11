4f969267ac02425d7720be00dc3c9d104c1b26ed
Mobeidexue <fyli88@qq.com>
2024-08-12 03:20:26 +0800

    Add Acceptor class

---
+++ muzi/net/acceptor.cc
@@ -0,0 +1,111 @@
+#include "acceptor.h"
+
+#include <errno.h>
+#include <functional>
+
+#include <fcntl.h>
+
+#include "logger.h"
+#include "socket.h"
+
+namespace muzi
+{
+
+namespace
+{
+int OpenIdleFile()
+{
+    int fd = ::open("/dev/null", O_RDONLY | O_CLOEXEC);
+    if (fd < 0)
+    {
+        LOG_SYSFAT << "::open() fails";
+    }
+    return fd;
+}
+
+}   // internal linkage
+
+Acceptor::Acceptor(EventLoop *loop, const InetAddress &listen_addr, 
+    bool reuse_port, const NewConnectionCallBack &cb)
+    : loop_(loop),
+      accept_socket_(socket::CreateNonBlockingSockOrDie()),
+      cb_(cb),
+      chanel_(loop, accept_socket_.GetFd()),
+      listening_(false),
+      dummy_fd_(OpenIdleFile())
+{
+    accept_socket_.SetReuseAddr(true);
+    if (reuse_port)
+        accept_socket_.SetReusePort(true);
+    accept_socket_.BindAddress(listen_addr);
+    chanel_.SetReadCallback(std::bind(&Acceptor::HandleRead, this));
+}
+
+Acceptor::~Acceptor()
+{
+    chanel_.DisableAll();
+    chanel_.Remove();
+    socket::Close(dummy_fd_);
+}
+
+void Acceptor::Listen()
+{
+    loop_->AssertInLoopThread();
+
+    listening_ = true;
+    accept_socket_.Listen();
+    chanel_.EnableReading();
+}
+
+void Acceptor::HandleRead()
+{
+    loop_->AssertInLoopThread();
+
+    int peer_sock;
+    InetAddress peer_addr;
+
+    while ((peer_sock = socket::Accept(accept_socket_.GetFd(), peer_addr.GetAddr())))
+    {
+        // The special problem of accept()ing when you can't
+        // Refer to this:
+        // http://pod.tst.eu/http://cvs.schmorp.de/libev/ev.pod#The_special_problem_of_accept_ing_wh
+        // 
+        // When the system's file descriptors are used up, the accept() will fail, but
+        // it still keep the listening socket fd readable, which will cause our eventloop to
+        // busy wait.
+        // 
+        // The graceful solution is that we can keep a dummy_fd_ to take up one file descriptor,
+        // when this case occurs, we close it to release one and accept the new connection,
+        // then we close the new conection, and set the dummy_fd_ again. This way we can 
+        // gracefully close the new connection when the system's file descriptors are exhausted.
+        //
+        // This solution is not thread-safe, which means only one thread can run to accept new
+        // connections using this solution. (Maybe a mutex_lock can make it thread-safe?)
+        if (peer_sock < 0)
+        {
+            if (errno == EMFILE || errno == ENFILE)
+            {
+                socket::Close(dummy_fd_);
+                dummy_fd_ = ::accept(accept_socket_.GetFd(), nullptr, nullptr);
+                socket::Close(dummy_fd_);
+                dummy_fd_ = OpenIdleFile();
+                continue;
+            }
+            else if (errno == EAGAIN)
+            {
+                break;
+            }
+        }   
+
+        if (cb_)
+        {
+            cb_(peer_sock, peer_addr);
+        }
+        else
+        {
+            socket::Close(peer_sock);
+        }
+    }
+}
+
+} // namespace muzi
---
+++ muzi/net/acceptor.h
@@ -0,0 +1,53 @@
+#ifndef MUZI_NET_ACCEPTOR_H_
+#define MUZI_NET_ACCEPTOR_H_
+
+#include <functional>
+
+#include "channel.h"
+#include "event_loop.h"
+#include "inet_address.h"
+#include "noncopyable.h"
+#include "socket.h"
+
+namespace muzi
+{
+/// @brief It is used by TcpServer to accept(2) new connection.
+class Acceptor : noncopyable
+{
+public:
+    typedef std::function<void(int sock_fd, 
+        const InetAddress &addr)> NewConnectionCallBack;
+    
+    Acceptor(EventLoop *loop, const InetAddress &listen_addr, 
+        bool reuse_port = false, const NewConnectionCallBack &cb = nullptr);
+
+    ~Acceptor();
+
+    void SetNewConnectionCallBack(const NewConnectionCallBack &cb)
+    {
+        cb_ = cb;
+    }
+
+    bool IsListening() const { return listening_; }
+
+    void Listen();
+
+private:
+    void HandleRead();
+
+private:
+    EventLoop *loop_;
+    Socket accept_socket_;
+    NewConnectionCallBack cb_;
+
+    Channel chanel_;
+    bool listening_;
+    // To take up a file descriptor, when system
+    // fd is used up, we can close it and accept the
+    // new connection and close it too.
+    int dummy_fd_;
+};
+
+}   // namespace muzi
+
+#endif // MUZI_NET_ACCEPTOR_H_
